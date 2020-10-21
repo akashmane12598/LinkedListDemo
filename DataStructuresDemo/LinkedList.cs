3b436411fc6205e40a5be93c1c259d30a8438b75
Akash <akashmane12598@gmail.com>
2020-10-22 00:14:18 +0530

    [Akash] Add. Master Code

---
+++ DataStructuresDemo/LinkedList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresDemo
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                
+            }
+            Console.WriteLine("Node is added " + node.data);
+        }
+
+        public void Display()
+        {            
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            while (temp!= null)
+            {
+                Console.Write(temp.data+" ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+
+        }
+
+        public void InsertData(int position, int data)
+        {
+            Console.WriteLine("Linked List after insertion: ");
+            int i = 1;
+            Node node = new Node(data);
+            
+            if (head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = this.head;
+                Node q = null;
+                while (i != position)
+                {
+                    q = temp;
+                    temp = temp.next;
+                    i++;
+                }
+                q.next = node;
+                node.next = temp;
+            }
+        }
+
+        public void DeleteDataAtPosition(int position)
+        {
+            int i = 1;
+            Node temp = this.head;
+            Node q = null;
+
+            while (i!=position)
+            {
+                q = temp;
+                temp = temp.next;
+                i++;
+            }
+            q.next = temp.next;
+        }
+    }
+}
---
+++ DataStructuresDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataStructuresDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Elements are:");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.InsertData(2, 40);
+            list.Display();
+            list.DeleteDataAtPosition(3);
+            list.Display();
+            
+        }
+    }
+}
